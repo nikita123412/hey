726ae1f1b6665b4b014b9173f400964743c1ae0f
nikita123412 <38855663+nikita123412@users.noreply.github.com>
2019-08-23 15:11:28 +0400

    1

--- task1/src/main/java/config/byboth/Main.java
+++ task1/src/main/java/config/byboth/Main.java
@@ -3,11 +3,9 @@ package config.byboth;
 import config.byboth.beans.B;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("configForBoth.xml");
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Config.class);
         B b = (B) applicationContext.getBean("b");
         System.out.println("B-class a-value is: " + b.getA().getAName());
